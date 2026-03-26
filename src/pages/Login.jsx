a68be3faf1228aedca2cc40ee5ddc1a69ff687bf
Giovana <minji.gong@hotmail.com>
2026-03-25 21:19:30 -0300

    atualização

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import logoLoopop from '../assets/logo.svg';
 export default function Login() {
   const navigate = useNavigate();
 
@@ -13,25 +13,27 @@ export default function Login() {
 
       <main className="w-full max-w-md space-y-12 z-10">
         {/* Brand Identity Section */}
-        <div className="text-center space-y-4">
-          <div className="inline-flex items-center justify-center w-20 h-20 rounded-xl signature-gradient text-white shadow-2xl mb-4 transform -rotate-6">
-            <span className="material-symbols-outlined text-4xl">shopping_bag</span>
+       <div className="text-center space-y-4">
+          <div className="flex flex-col items-center text-center z-10 w-full max-w-md">
+            <img
+                          alt="Loopop Brand Logo"
+                          className="w-32 h-32 md:w-40 md:h-40 object-contain relative z-10"
+                          src={logoLoopop}
+                        />
           </div>
-          <h1 className="font-headline text-5xl font-extrabold tracking-tight text-primary">Loopop</h1>
-          <p className="text-on-surface-variant font-medium text-lg">Seu marketplace local favorito</p>
-        </div>
+          </div>
 
         {/* Login Form Container */}
-        <div className="space-y-8">
-          <div className="grid grid-cols-1 gap-6">
+        <div className="w-full max-w-sm space-y-8">
+          <div className="grid grid-cols-1 gap-5">
             
             {/* Email Input */}
-            <div className="space-y-2">
-              <label class="text-sm font-bold uppercase tracking-wider text-on-surface-variant ml-2" for="email">E-mail</label>
-              <div className="relative">
+            <div className="space-y-1">
+              <label className="text-sm font-bold uppercase tracking-wider text-on-surface-variant" htmlFor="email"> Email </label>
+              <div className="relative group">
                 <span className="absolute left-4 top-1/2 -translate-y-1/2 material-symbols-outlined text-on-surface-variant">mail</span>
                 <input 
-                  className="w-full pl-12 pr-4 py-4 bg-surface-container-lowest rounded-lg border-none focus:ring-2 focus:ring-primary text-on-surface font-medium placeholder:text-outline-variant transition-all duration-300 outline-none" 
+                  className="w-full pl-12 pr-12 py-4 bg-white rounded-lg shadow-sm border-primary-400 focus:ring-2 focus:ring-primary/20 text-on-surface font-medium placeholder:text-outline-variant transition-all duration-300 outline-none" 
                   id="email" 
                   placeholder="nome@exemplo.com" 
                   type="email"
@@ -43,12 +45,12 @@ export default function Login() {
             <div className="space-y-2">
               <div className="flex justify-between items-center ml-2">
                 <label className="text-sm font-bold uppercase tracking-wider text-on-surface-variant" htmlFor="password">Senha</label>
-                <button className="text-sm font-bold text-secondary hover:opacity-80 transition-opacity">Esqueci minha senha</button>
+                <button className="text-sm font-bold text-secondary hover:opacity-50 transition-opacity">Esqueci minha senha</button>
               </div>
               <div className="relative">
                 <span className="absolute left-4 top-1/2 -translate-y-1/2 material-symbols-outlined text-on-surface-variant">lock</span>
                 <input 
-                  className="w-full pl-12 pr-12 py-4 bg-surface-container-lowest rounded-lg border-none focus:ring-2 focus:ring-primary text-on-surface font-medium placeholder:text-outline-variant transition-all duration-300 outline-none" 
+                  className="w-full pl-12 pr-12 py-4 bg-white rounded-lg shadow-sm border-primary-400 focus:ring-2 focus:ring-primary/20 text-on-surface font-medium placeholder:text-outline-variant transition-all duration-300 outline-none" 
                   id="password" 
                   placeholder="••••••••" 
                   type="password"
@@ -85,9 +87,9 @@ export default function Login() {
         </div>
       </main>
 
-      {/* Support Message (Floating Support) */}
+      {/* Support Message (Floating Support) 
       <div className="fixed bottom-8 left-1/2 -translate-x-1/2 w-[90%] max-w-sm glass-panel p-4 rounded-lg flex items-center gap-4 shadow-xl border border-white/20">
-        <div className="w-10 h-10 rounded-full bg-primary-container flex items-center justify-center text-primary">
+       <div className="w-10 h-10 rounded-full bg-primary-container flex items-center justify-center text-primary">
           <span className="material-symbols-outlined">help_center</span>
         </div>
         <div className="flex-1">
@@ -95,7 +97,7 @@ export default function Login() {
           <p className="text-sm font-bold text-on-surface">Fale com nosso suporte 24/7</p>
         </div>
         <span className="material-symbols-outlined text-outline-variant">chevron_right</span>
-      </div>
+      </div>*/}
     </div>
   );
 }
